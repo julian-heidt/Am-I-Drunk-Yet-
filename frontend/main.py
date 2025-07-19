edfeb90d3ee0a86a5da982b4269ea04141b8eda9
beasticle <julian.heidt@gmail.com>
2025-07-19 13:16:24 -0400

    added health checks to see if backend or frontend is the issue

--- frontend/main.py
+++ frontend/main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return '', 204
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001) 
