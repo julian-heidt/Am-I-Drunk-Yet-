fef2f77df170a7403a33963dc7a2224be85da174
beasticle <julian.heidt@gmail.com>
2025-07-19 15:28:45 -0400

    changing ports babyyyy

--- backend/main.py
+++ backend/main.py
@@ -66,4 +66,4 @@ async def calculate_api(request: CalculationRequest):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    uvicorn.run(app, host="0.0.0.0", port=5050) 
